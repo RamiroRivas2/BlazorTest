e57eed872f478100c56b24194a4911e62d75b725
RamiroRivas2 <125583927+RamiroRivas2@users.noreply.github.com>
2025-06-16 12:24:47 -0400

    Validation added for email and phone

--- BlazorApp/Entities/Client.cs
+++ BlazorApp/Entities/Client.cs
@@ -1,10 +1,17 @@
-﻿namespace BlazorTest.Entities
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorTest.Entities
 {
     public class Client
     {
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; } = string.Empty;
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Please enter a valid email address")]
         public string Email { get; set; } = string.Empty;
+        [Required]
+        [RegularExpression(@"^\([\d]{3}\) [\d]{3}-[\d]{4}$", ErrorMessage = "Phone number must be in the format (123) 456-7890")]
         public string Phone { get; set; } = string.Empty;
     }
 }
